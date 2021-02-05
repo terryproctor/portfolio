d3d27fc69f7ac2fc53fbdf5f60fa13143116af76
terryproctor <terryproctor2015@outlook.com>
2021-02-05 22:59:08 +0000

    line 12 js null error

--- script.js
+++
@@ -1,5 +0,0 @@
-const openWindowButton = document.querySelectorAll('[data-window-target]')
-const closeWindowButton = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay');
-
-                                                   
