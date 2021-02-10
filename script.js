09b5b063755bc92cf9bce6078cc1e378f866ad15
terryproctor <terryproctor2015@outlook.com>
2021-02-10 21:53:51 +0000

    burger open, and about bugs fixed

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal");
+var modal2 = document
 
 // Get the button that opens the modal
 var btn1 = document.getElementsByClassName("open-window")[0];
+var btn2 = document.getElementsByClassName("open-window")[1];
+var btn3 = document.getElementsByClassName("open-window")[2];
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {
-  modal.style.display = "block";
+  modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
   }
 }
 
 
 //////////////////////////////////////////
 //burger menu code//
+var navLinks = document.getElementsByClassName("nav_link");
+var navBar = document.getElementsByClassName("navBar")[0];
+var burgerButton = document.getElementById("burgerWrap");
 
-var burgerButton = document.getElementsByClassName("logo_nav_link")[0]
 
-burgerButton.addEventListener('click', openDownMenu());
+burgerButton.addEventListener('click', openDownMenu);
 
 function openDownMenu() {
-  
+navBar.style.display = "contents";
+
+for (var i=0; i<navLinks.length; i++) {
+navLinks[i].style.display= "block";
+}
+
 }
